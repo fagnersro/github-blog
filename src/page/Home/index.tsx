dbe0a94e632df96b8bf61c59cd77947fe3b04c65
fagnersro <fagner.hg40@gmail.com>
2023-04-20 13:09:25 -0300

    Visual structure - Input

--- src/page/Home/index.tsx
+++ src/page/Home/index.tsx
@@ -1,10 +1,27 @@
 import Aside from './components/Aside'
-import { ContainerHomer } from './styles'
+import {
+  ContainerHomer,
+  ContinerInput,
+  DowContentInput,
+  TopContentInput,
+} from './styles'
 
 export default function Home() {
   return (
     <ContainerHomer>
       <Aside />
+
+      <ContinerInput>
+        <TopContentInput>
+          <span>Publicações</span>
+          <div>
+            <span>6 publicações</span>
+          </div>
+        </TopContentInput>
+        <DowContentInput>
+          <input type="text" placeholder="Buscar conteúdo" />
+        </DowContentInput>
+      </ContinerInput>
     </ContainerHomer>
   )
 }
--- src/page/Home/styles.ts
+++ src/page/Home/styles.ts
@@ -1,7 +1,7 @@
 import { styled } from '../../styles'
 
 export const ContainerHomer = styled('div', {
-  border: '1px solid red',
+  // border: '1px solid red',
   display: 'flex',
   flexDirection: 'column',
 
@@ -9,4 +9,74 @@ export const ContainerHomer = styled('div', {
   marginTop: 'calc(0px - 5.8rem - 6px)',
   maxWidth: '54.1rem',
   width: '100%',
+})
+
+export const ContinerInput = styled('label', {
+  height: '5.688rem',
+
+  display: 'flex',
+  flexDirection: 'column',
+  justifyContent: 'space-between',
+
+  marginTop: '4.75rem',
+  maxWidth: '54.1rem',
+  width: '100%',
+})
+
+export const TopContentInput = styled('div', {
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'space-between',
+
+  height: '1.813rem',
+  maxWidth: '54.1rem',
+  width: '100%',
+
+  span: {
+    color: '$baseSubtitle',
+    fontStyle: 'normal',
+    fontWeight: 700,
+    fontSize: '18px',
+    lineHeight: '160%',
+  },
+
+  div: {
+    height: '1.813rem',
+
+    span: {
+      color: '$baseSpan',
+      fontStyle: 'normal',
+      fontWeight: 400,
+      fontSize: '14px',
+      lineHeight: '160%',
+    },
+  },
+})
+
+export const DowContentInput = styled('div', {
+  backgroundColor: '$baseInput',
+  border: '1px solid $baseBorder',
+  borderRadius: '6px',
+
+  display: 'flex',
+  padding: '12px 16px',
+
+  height: '3.125rem',
+  maxWidth: '54.1rem',
+  width: '100%',
+
+  input: {
+    all: 'unset',
+    color: '$baseLabel',
+
+    fontStyle: 'normal',
+    fontWeight: 400,
+    fontSize: '16px',
+    lineHeight: '160%',
+    flex: 1,
+
+    '&::placeholder': {
+      color: '$baseLabel',
+    },
+  },
 })
